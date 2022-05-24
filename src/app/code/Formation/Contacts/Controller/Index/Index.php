dfb2bff97b1be365a6277cca7f008b7fc39a7eff
Wiam Zellou <sqli.interns3@gmail.com>
2022-05-24 18:26:12 +0100

    magento 2 env

---
+++ src/app/code/Formation/Contacts/Controller/Index/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Formation\Contacts\Controller\Index;
+
+use Magento\Framework\App\ResponseInterface;
+
+/**
+ * Class Index
+ * @package Formation\Contacts\Controller\Index
+ */
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    /**
+     * Excute action for testing controller ..Etc
+     * Execute action based on request and return result
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        die('test index');
+    }
+}
